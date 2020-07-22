3765ef909947c2a39e33ed537f89591159ed3f96
boko999 <ho-oh999@hotmail.com>
2020-07-22 19:28:45 +0000

    alot

--- main.py
+++ main.py
@@ -15,15 +15,25 @@ def triangle(length, color):
   while x <3:
     bob.forward(int(length))
     bob.right(120)
-    x = b.end_fill()
+    x = 0
+  bob.end_fill()
+def circle(length, color):
+  bob.fillcolor(color)
+  bob.begin_fill()
+  x = 0
+  while x <100:
+    bob.forward(int(length))
+    bob.right(2)
+    x = 0
+    bob.end_fill()
 
 input_shape = input("what shape do you want to draw?")
 input_length = input("choose how big: ")
 input_color = input("choose color: ")
 
 if input_shape.lower() == "triangle":
-  print ("triangle")
   triangle(input_length, input_color)
+
 elif input_shape.lower() =="star":
   print ("star")
 
